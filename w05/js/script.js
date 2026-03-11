72e7e574be2545f4a9713df51882fb1a42e8c51c
mels-andrade <andradem@ocadu.ca>
2026-03-11 18:02:19 -0400

    w05 (final)

--- w05/js/script.js
+++ w05/js/script.js
@@ -8,3 +8,23 @@ $("#preamble-btn-2").click(function(){
 $(".header-left").click(function(){
     $("#menu").toggleClass("active");
 })
+
+$(".album-thumbs").flickity({
+    wrapAround: true,
+    pageDots: false, 
+    /*let thumbnail = $('.carousel').flickity({
+        fade: true; <-- not working
+    })*/ 
+})
+
+
+/*$(document).scroll(function(){
+    let distanceScrolled = $(document).scrollTop();
+    //let height = $(document).height() - window.height();
+    //let ratio = distanceScrolled / height; <-- not working, written wrong
+    $(".header-right").html("you have traveled: " +distanceScrolled + " pixels.");
+    $(".h1").css({
+
+    });
+    //console.log(distanceScrolled);
+})*/
