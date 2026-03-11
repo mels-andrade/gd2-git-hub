8a8dbc7c7e1efc55c4bde15cd558555785739bee
mels-andrade <andradem@ocadu.ca>
2026-03-11 16:52:08 -0400

    w05 (2)

---
+++ w05/js/script.js
@@ -0,0 +1,10 @@
+$("#preamble-btn-2").click(function(){
+    //alert("alert");
+    $("#preamble-btn-1").css({
+        "rotate":"45deg"
+    });
+})
+
+$(".header-left").click(function(){
+    $("#menu").toggleClass("active");
+})
